d1780ce1b51334a858f042a0b5b65afa904d7cd6
Kevin Welch <kj01a@yahoo.com>
2016-08-17 16:59:45 -0500

    Add files via upload

--- AverageVarianceDeviation.py
+++ AverageVarianceDeviation.py
@@ -1,15 +1,16 @@
-def grades_average(grades):
-    sum_of_grades = grades_sum(grades)
-    average = sum_of_grades / float(len(grades))
+#Average of a list
+def average(x):
+    sum += x
+    average = sum / float(len(x))
     return average
 
-def grades_variance(scores):
-    average = grades_average(scores)
+#Variance of a list
+def variance(x):
+    avg = average(x)
     variance = 0
-    for score in scores:
-        variance += (average - score) ** 2
-    return variance / len(scores)
-print grades_variance(grades)
+    for i in x:
+        variance += (avg - i) ** 2
+    return variance / len(x)
 
 def grades_std_deviation(variance):
     return variance ** 0.5
